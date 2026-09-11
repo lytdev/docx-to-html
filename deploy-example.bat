e0cad3e7b2cd01575f8b71338a9ff21175254300
lytdev <lytdev@outlook.com>
2026-09-11 10:56:55 +0800

    更新设置

--- deploy-example.bat
+++ deploy-example.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul 2>&1
 
 set JAVA_HOME=C:\DevRepo\OpenJdk\dragonwell-21.0.11
 
-set MVN=C:\DevRepo\maven\apache-maven-3.8.8\bin\mvn.cmd
+set MVN=C:\DevRepo\Maven\Maven39\bin\mvn.cmd
 
 set MAVEN_GPG_PASSPHRASE=你的密码
 
